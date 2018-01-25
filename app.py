de9283840713cdf1e282e74e769b0aae1604307f
Shreyaansh Bassi <sbassi@purdue.edu>
2018-01-25 01:53:05 -0500

    Test 1

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return('Hello World!')
 
+@app.route('/yolo')
+def yolo():
+    return('Yolo World!')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
