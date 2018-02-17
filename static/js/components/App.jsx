51065de975be75301f3273885e7cc1f764b14a5e
yashshiroya <yshiroya@purdue.edu>
2018-02-16 20:09:47 -0500

    Login works, gets access token, persistence to be added

--- static/js/components/App.jsx
+++ static/js/components/App.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { GoogleLogin } from 'react-google-login';
 
 import goToLogin from '../actions/action_select_login'
 import goToMain from '../actions/action_select_main'
 
+const clientId = "";
+
 class App extends React.Component {
+
+	constructor (props) {
+		super(props);
+	}
+
+	responseGoogle(response) {
+		console.log(response);
+	}
+
 	render() {
+
 		return (
-			<div><h1>🏨 Travlr ✈️</h1></div>
+			<div>
+				
+				<h2>🏨 Travlr ✈️</h2>
+
+				<GoogleLogin
+					clientId="110941707391-lin5grtvjtedoudnpe5p37tnbq7f3qkd.apps.googleusercontent.com"
+					buttonText="Sign-in with Google"
+					onSuccess={this.responseGoogle}
+					onFailure={this.responseGoogle}
+				/>
+			
+			</div>
 		);
 	}
 
