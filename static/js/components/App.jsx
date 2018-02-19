684d17245144d8e46e63fc425c9b113e781bb825
yashshiroya <yshiroya@purdue.edu>
2018-02-19 03:35:15 -0500

    Users stored to localStorage object{} in browser to keep persistently logged-in

--- static/js/components/App.jsx
+++ static/js/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { GoogleLogin } from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 
 import goToLogin from '../actions/action_select_login'
 import goToMain from '../actions/action_select_main'
@@ -12,10 +13,40 @@ class App extends React.Component {
 
 	constructor (props) {
 		super(props);
+
+		this.state = {
+			currentUser: JSON.parse(localStorage.getItem('currentUser')) || null
+		};
+		
+		this.googleLogin = this.googleLogin.bind(this);
+		this.googleLogout = this.googleLogout.bind(this);
+		this.nameHandler = this.nameHandler.bind(this);
+
 	}
 
-	responseGoogle(response) {
+	googleLogin(response) {
+		console.log("logged in");
 		console.log(response);
+		if(response) {
+			localStorage.setItem('currentUser', JSON.stringify(response));
+			this.setState({currentUser: JSON.parse(localStorage.getItem('currentUser'))});
+		}
+		else {
+			console.log("Error: googleLogin@App.jsx");
+		}
+	}
+
+	googleLogout(){
+		localStorage.clear();
+		this.setState({currentUser: ""});
+	}
+
+
+	nameHandler() {
+		if( this.state.currentUser && this.state.currentUser.w3) {
+			return this.state.currentUser.w3.ig;
+		}
+		else return ""
 	}
 
 	render() {
@@ -25,13 +56,20 @@ class App extends React.Component {
 				
 				<h2>🏨 Travlr ✈️</h2>
 
+				<hr />
+				<h5> Logged in as {this.nameHandler()}.</h5>
 				<GoogleLogin
 					clientId="110941707391-lin5grtvjtedoudnpe5p37tnbq7f3qkd.apps.googleusercontent.com"
 					buttonText="Sign-in with Google"
-					onSuccess={this.responseGoogle}
-					onFailure={this.responseGoogle}
+					onSuccess={this.googleLogin}
+					onFailure={this.googleLogin}
 				/>
-			
+
+				<GoogleLogout
+					buttonText="Logout"
+					onLogoutSuccess={this.googleLogout}
+				/>
+
 			</div>
 		);
 	}
