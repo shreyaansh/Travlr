aed94acd0d3fb47c374a0dc666e0549f20daad47
Shreyaansh Bassi <sbassi@purdue.edu>
2018-02-21 00:44:03 -0500

    Modified constants

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,5 @@
 export default {
 				routeUrl: "http://localhost:5000/",
+				routeDistUrl: "https://travlr-io.herokuapp.com/",
 				clientId: "110941707391-lin5grtvjtedoudnpe5p37tnbq7f3qkd.apps.googleusercontent.com"
 }
