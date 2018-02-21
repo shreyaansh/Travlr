dec11e650505c78c3cfc2f463275b759b6fe106f
Shreyaansh Bassi <sbassi@purdue.edu>
2018-02-20 22:37:42 -0500

    Commenting google code

--- app.py
+++ app.py
@@ -19,25 +19,25 @@ def authenticate():
     token = request.get_json()
     print(token)
     ret_token = { "token" : "SUCCESS" }
-    try:
+    #try:
         # Specify the CLIENT_ID of the app that accesses the backend:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+    #    idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
         # Or, if multiple clients access the backend server:
         # idinfo = id_token.verify_oauth2_token(token, requests.Request()) # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
         #     raise ValueError('Could not verify audience.')
         
-        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            raise ValueError('Wrong issuer.')
+    #    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+    #        raise ValueError('Wrong issuer.')
 
         # If auth request is from a G Suite domain:
         # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
         #     raise ValueError('Wrong hosted domain.')
         # ID token is valid. Get the user's Google Account ID from the decoded token.
-        userid = idinfo['sub']
-    except ValueError:
+    #    userid = idinfo['sub']
+    #except ValueError:
         # Invalid token
-        pass
+    #    pass
     
     return jsonify(ret_token) 
 
