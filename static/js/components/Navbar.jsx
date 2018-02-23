3bc0c36dd3b19c6e5f97fd6a04ab759cfbebb707
Aditya Dhingra <adityadhingra07@gmail.com>
2018-02-22 20:08:52 -0500

    UI UI UI

--- static/js/components/Navbar.jsx
+++ static/js/components/Navbar.jsx
@@ -12,58 +12,60 @@ class Navbar extends React.Component {
     }
 
     render() {
-        return(
-            <div className="navbar-fixed">
-                <nav className="black">
-                <div className="nav-wrapper">
-                <a href="#!" className="brand-logo"><img src="/assets/travlr_logo_cropped_small.png" /></a>
-                <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                <ul className="right hide-on-med-and-down">
-                    <li><a href="#">Sass</a></li>
-                    <li><a href="#">Components</a></li>
-                    <li>
-                    {!this.props.navProps.nameHandler
-                        
-                        ?
-                            <GoogleLogin
-                                clientId="110941707391-lin5grtvjtedoudnpe5p37tnbq7f3qkd.apps.googleusercontent.com"
-                                buttonText="Sign-in with Google"
-                                onSuccess={this.props.navProps.login}
-                                onFailure={this.props.navProps.login}
-                            />
-                        : 
-                            <GoogleLogout
-                                buttonText={"Logout of " + this.props.navProps.nameHandler}
-                                onLogoutSuccess={this.props.navProps.logout}
-                            />
-                    }
-                    </li>
-                </ul>
-                </div>
-            </nav>
-            
-            <ul className="sidenav" id="mobile-demo">
-                <li><a href="#">Sass</a></li>
-                <li><a href="#">Components</a></li>
-                <li>
-                    {!this.props.navProps.nameHandler
+        return (
+            <div>
+                <div className="navbar-fixed">
+                    <nav className="black">
+                        <div className="nav-wrapper">
+                            <a href="#" className="brand-logo"><img id="nav_logo" src="/assets/travlr_logo_white.png"/></a>
+                            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i
+                                className="material-icons">menu</i></a>
+                            <ul className="right hide-on-med-and-down">
+                                <li><a href="#"><i className="material-icons left">settings</i></a></li>
+                                <li>
+                                    {!this.props.navProps.nameHandler
 
-                        ?
-                        <GoogleLogin
-                            clientId="110941707391-lin5grtvjtedoudnpe5p37tnbq7f3qkd.apps.googleusercontent.com"
-                            buttonText="Sign-in with Google"
-                            onSuccess={this.props.navProps.login}
-                            onFailure={this.props.navProps.login}
-                        />
-                        :
-                        <GoogleLogout
-                            buttonText={"Logout of " + this.props.navProps.nameHandler}
-                            onLogoutSuccess={this.props.navProps.logout}
-                        />
-                    }
-                </li>
-            </ul>
-          </div>
+                                        ?
+                                        <GoogleLogin
+                                            clientId="110941707391-lin5grtvjtedoudnpe5p37tnbq7f3qkd.apps.googleusercontent.com"
+                                            buttonText="Sign-in with Google"
+                                            onSuccess={this.props.navProps.login}
+                                            onFailure={this.props.navProps.login}
+                                        />
+                                        :
+                                        <GoogleLogout
+                                            buttonText={"Logout of " + this.props.navProps.nameHandler}
+                                            onLogoutSuccess={this.props.navProps.logout}
+                                        />
+                                    }
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div>
+                    <ul className="sidenav" id="mobile-demo">
+                        <li><a href="#"><i className="material-icons left">settings</i>Settings</a></li>
+                        <li>
+                            {!this.props.navProps.nameHandler
+
+                                ?
+                                <GoogleLogin
+                                    clientId="110941707391-lin5grtvjtedoudnpe5p37tnbq7f3qkd.apps.googleusercontent.com"
+                                    buttonText="Sign-in with Google"
+                                    onSuccess={this.props.navProps.login}
+                                    onFailure={this.props.navProps.login}
+                                />
+                                :
+                                <GoogleLogout
+                                    buttonText={"Logout of " + this.props.navProps.nameHandler}
+                                    onLogoutSuccess={this.props.navProps.logout}
+                                />
+                            }
+                        </li>
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
