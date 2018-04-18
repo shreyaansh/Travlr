86d8c718eb0be6a7434efa862d74f0f0fef600bb
Aditya Dhingra <adityadhingra07@gmail.com>
2018-04-17 22:55:13 -0400

    Developer button added

---
+++ static/js/components/Developer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Developer = (props) => {
+    return (
+        <div>
+            <a className='waves-effect waves-light btn' href='#' id="developer_btn"><i className="material-icons left">developer_mode</i>Developer Settings</a>
+        </div>
+    );
+}
+
+export default Developer;
