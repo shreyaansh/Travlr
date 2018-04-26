f036f0380b7b9de646fb96e84585a99ee404c5af
jbless4c89 <jbless4c89@gmail.com>
2018-04-25 22:35:28 -0400

    PDF generation
    PDF generation

--- static/js/components/Itinerary.js
+++ static/js/components/Itinerary.js
@@ -24,7 +24,37 @@ class Itinerary extends React.Component {
 
     render() {
         var locations = this.locationList();
-
+        var pl = this.props.currentSelection.itinerary;
+        var doc = new jsPDF();
+        doc.text("Your Itinerary:",10,10);
+        var y = 20;
+        jQuery.each(pl, function(loc, locs){
+            doc.text(loc,10,y);
+            y+=10;
+            doc.text("Hotel Information:",20,y);
+            y+=10;
+            var hn = locs.selectedHotel.name;
+            doc.text(hn,30,y);
+            y+=10;
+            var ha = locs.selectedHotel.formatted_address;
+            doc.text(ha,40,y);
+            y+=10;
+            var se = locs.selectedEvents;
+            doc.text("Events you are interested in:",20,y);
+            y+=10;
+            jQuery.each(se,function(eid,einf){
+                var et = einf.title;
+                doc.text(et, 30, y);
+                y+=10;
+                var en = einf.venue_name;
+                doc.text(en,40,y);
+                y+=10
+                var est = einf.start_time;
+                doc.text(est,40,y);
+                y+=10;
+            });
+        });
+        doc.save("itin.pdf");
         return (
             <div id="itinerary">
                 <h2 id="opt_tile">Here's your Itinerary.</h2>
